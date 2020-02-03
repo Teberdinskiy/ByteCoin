9c69aebad683689ac53df80e051533cae59d2983
Alikhan Batchaev <loko-han@mail.ru>
2020-02-03 12:06:52 +0300

    передача данных в пикер вью

---
+++ ByteCoin/Model/CoinManager.swift
@@ -0,0 +1,19 @@
+//
+//  CoinManager.swift
+//  ByteCoin
+//
+//  Created by 🧔🏻Alikhan Batchaev on 03.02.2020.
+//  Copyright © 2020 Alikhan Batchaev. All rights reserved.
+//
+
+import Foundation
+
+struct CoinManager {
+    
+    let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    
+    func getCoinPrice(for currency: String) {
+        
+    }
+}
