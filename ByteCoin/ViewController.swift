531a8183959a8b7bbbead4eb0bbb4536cd534e26
Alikhan Batchaev <loko-han@mail.ru>
2020-02-03 10:49:35 +0300

    Initial Commit

---
+++ ByteCoin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ByteCoin
+//
+//  Created by 🧔🏻Alikhan Batchaev on 03.02.2020.
+//  Copyright © 2020 Alikhan Batchaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
